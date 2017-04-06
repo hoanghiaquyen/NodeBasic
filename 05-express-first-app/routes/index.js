93828e6353b1ca4cf9ff149d94d3630135621184
HoangQuyen <hoangquyenit162@outlook.com>
2017-04-06 19:45:13 +0700

    configuration file router/users.js in router/index.js

--- 05-express-first-app/routes/index.js
+++ 05-express-first-app/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//configuration file router/users.js in router/index.js 
+router.get('/users', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/users/detail', function(req, res, next) {
+  res.send('User detail');
+});
 module.exports = router;
