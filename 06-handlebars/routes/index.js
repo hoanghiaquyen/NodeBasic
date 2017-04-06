141f40ebaa736758b0ba05c18803c14ebd095909
HoangQuyen <hoangquyenit162@outlook.com>
2017-04-06 23:18:49 +0700

    Handlebars Templating engine

---
+++ 06-handlebars/routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', condition: false, anyArray: [1,2,3] });
+});
+
+//configuration file router/users.js in router/index.js 
+// router.get('/users', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// router.get('/users/detail', function(req, res, next) {
+//   res.send('User detail');
+// });
+module.exports = router;
